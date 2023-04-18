c5e39cb56a00e60ff5319694087007f2fa911c4f
sivilenok <cherepahasivi@gmail.com>
2023-04-18 18:19:24 +0200

    fix test

--- src/pages/CatalogPage/CatalogPage.test.tsx
+++ src/pages/CatalogPage/CatalogPage.test.tsx
@@ -1,12 +1,68 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { Button } from './Button';
+import { render, screen } from '@testing-library/react';
+import { CatalogPage } from './CatalogPage';
 
-test('Button calls onClick function when clicked', () => {
-  const mockOnClick = jest.fn();
-  const buttonText = 'Click me!';
-  render(<Button onClick={mockOnClick} />);
-  const button = screen.getByText(buttonText);
-  fireEvent.click(button);
-  expect(mockOnClick).toHaveBeenCalled();
+describe('CatalogPage', () => {
+  test('renders mobile view correctly', () => {
+    // Arrange
+    window.innerWidth = 500;
+
+    // Act
+    render(<CatalogPage />);
+
+    // Assert
+    const title = screen.getByText('Косметика и гигиена');
+    expect(title).toBeInTheDocument();
+    const productList = screen.getByRole('list');
+    expect(productList).toBeInTheDocument();
+    const pagination = screen.getByRole('navigation');
+    expect(pagination).toBeInTheDocument();
+  });
+
+  test('renders desktop view correctly', () => {
+    // Arrange
+    window.innerWidth = 1024;
+
+    // Act
+    render(<CatalogPage />);
+
+    // Assert
+    const title = screen.getByText('Косметика и гигиена');
+    expect(title).toBeInTheDocument();
+    const filters = screen.getAllByRole('button');
+    expect(filters.length).toBeGreaterThan(0);
+    const productList = screen.getByRole('list');
+    expect(productList).toBeInTheDocument();
+    const pagination = screen.getByRole('navigation');
+    expect(pagination).toBeInTheDocument();
+  });
+
+  test('can filter products by category', () => {
+    // Arrange
+    render(<CatalogPage />);
+    const filters = screen.getAllByRole('button');
+    const filterToClick = filters[0];
+
+    // Act
+    filterToClick.click();
+
+    // Assert
+    const products = screen.getAllByRole('listitem');
+    const firstProduct = products[0];
+    expect(firstProduct).toHaveTextContent('Уход за телом');
+  });
+
+  test('can change pagination page', () => {
+    // Arrange
+    render(<CatalogPage />);
+    const nextPageButton = screen.getByTitle('Следующая страница');
+
+    // Act
+    nextPageButton.click();
+
+    // Assert
+    const currentPageButton = screen.getByRole('button', { name: /2/i });
+    expect(currentPageButton).toHaveAttribute('aria-current', 'page');
+  });
 });
+
 
